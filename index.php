6f13efda219f6f49a2e5efb19820dff1a2966b04
LordPompax <capitaine_ted@hotmail.fr>
2019-09-28 02:07:02 +0200

    1er commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>CV - teddy GAUTHIER</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="design.css">
+	</head>
+	<body>
+		<header>
+			<div class="large header">
+				<div class = "info_perso">
+					<span class = "info nom">Teddy GAUTHIER</span>
+					<span class = "info ville">Pontault-Combault 77340</span>
+					<span class = "info num">0777984056</span>
+					<span class = "info email">teddy.gauthier@outlook.com</span>
+					<span class = "info age">20 ans</span>
+				</div>
+				<img src = "Moi.jpg" class = "info_photo">
+			</div>
+		</header>
+		<nav class = "objectif">
+			<div class = "large centre">
+				<span class = "important">Objectif : </span>Stage de développement informatique
+			</div>
+		</nav>
+		<section>
+			<div class="large corps">
+				<!-- <div class = "top">
+					<div class = "fen">
+						<div class = "fen_titre">Description</div>
+						<p class = "fen_contenu">
+							merde
+						</p>
+					</div>
+				</div> -->
+				<div class = "corps2">
+					<div class = "main">
+						<div class = "fen">
+							<div class = "fen_titre">Competence</div>
+							<p class = "fen_contenu">
+								<span class = "important">Developpeur web Full-stack :</span> PHP, HTML, CSS, SQL, JS, Jquery, NodeJS<br/>
+								<span class = "important">Autre langage :</span> C, C++, Java, Python, Lua, BrainFuck<br/>
+								Préférence pour le developpement back-end
+							</p>
+						</div>
+						<div class = "fen">
+							<div class = "fen_titre">Projet personnel</div>
+							<p class = "fen_contenu">
+								en attente
+							</p>
+						</div>
+						<div class = "fen">
+							<div class = "fen_titre">Description</div>
+							<p class = "fen_contenu">
+								en attente
+							</p>
+						</div>
+						<div class = "fen">
+							<div class = "fen_titre">Interet</div>
+							<p class = "fen_contenu">
+								en attente
+							</p>
+						</div>
+					</div>
+					<div class = "second">
+						<div class = "fen">
+							<div class = "fen_titre_sec">Experience pro</div>
+							<p class = "fen_contenu">
+								<ul>
+									<li>Vente d'un site d'annonce en tant que freelance</li>
+									<li>Tentative de lancement de 2 sites a but lucratif avec un coéquipier (échec)</li>
+								</ul>
+								
+							</p>
+						</div>
+						<div class = "fen">
+							<div class = "fen_titre_sec">Etude</div>
+							<p class = "fen_contenu">
+								<ul>
+									<li>DUT informatique Université Paris XIII</li>
+									<li>baccalauréat STI2D</li>
+								</ul>
+								
+							</p>
+						</div>
+						<div class = "fen">
+							<div class = "fen_titre_sec">Contact</div>
+							<p class = "fen_contenu">
+								en attente
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--<footer>
+			<div class="large">
+				merde1
+			</div>
+		</footer>-->
+	</body>
+</html>
