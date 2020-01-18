10c33dc2a32c5d350b64390b37b7ed81eb7e539e
LordPompax <capitaine_ted@hotmail.fr>
2020-01-18 20:30:20 +0100

    truc

--- include/bdd.php
+++ include/bdd.php
@@ -1,4 +1,4 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=CV', 'root', '');
-	//ini_set("display_errors",0);error_reporting(0);
+	// $bdd = new PDO('mysql:host=localhost;dbname=CV', 'root', '');
+	// ini_set("display_errors",0);error_reporting(0);
 ?>
